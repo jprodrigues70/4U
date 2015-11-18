7ebd613862ce9da83104f2d37c455831f6584383
Bruno Oliveira <brunooliveira@infojr.com.br>
2015-11-18 17:51:57 -0300

    condicao de seguir disciplina no resultado da busca

--- controllers/discipline.php
+++ controllers/discipline.php
@@ -74,9 +74,12 @@
                         <div class="discipline-header">
                             <img src="../assets/img/ppd.jpeg" alt="">
                             <h4>'.$discipline->code.'</h4>
-                            <span>'.$discipline->name.'</span>
-                            <a class="btn btn-default" onclick="followDiscipline('.$discipline->id.', this)">Seguir</a>
-                        </div>
+                            <span>'.$discipline->name.'</span>'.
+                            ((UserHasDisciplines::exists($_SESSION['user'],$discipline->id)) ?
+                                '<button class="btn btn-default" onclick="unfollowDiscipline('.$discipline->id.', this)">Deixar de Seguir</button>'
+                            :
+                                '<button class="btn btn-default" onclick="followDiscipline('.$discipline->id.', this)">Seguir</button>')
+                        .'</div>
                     </div>';
                 }
             }
